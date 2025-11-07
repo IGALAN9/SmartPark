22972db89c68bb2382f0988dc160f8914419c9ae
Galang <Galangafdalahharsa88@gmail.com>
2025-11-07 22:53:43 +0700

    simple bug fixes

--- src/Frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XH/xpp2N7r8JxEFyfMUH+wwq/m47FsIiXgYybqdp4wE=\"\n}"
--- src/Frontend/.next/dev/static/chunks/src_Frontend_app_layout_tsx_3873bb71._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_Frontend_app_globals_1b01ef3d.css"
-],
-    source: "dynamic"
-});
